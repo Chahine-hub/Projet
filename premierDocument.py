610334cc37f28ce44c3e55a47c1e0e7e85e382b6
Chahine Bouchelaghem <riohanma1@gmail.com>
2026-02-10 15:23:55 +0100

    essais if else while

--- premierDocument.py
+++ premierDocument.py
@@ -1,5 +1,12 @@
 print("Hello world")
+x = int(input("Enter a number : "))
 if x<0 :
     print('yes')
 else :
     print('no')
+while x>0 :
+    x = int(input("Enter a number : "))
+    if x<0 :
+        print('yes')
+    else :
+        print('no')
